4a2e2848a017b84809a6a3c19f729a0f90a403ab
Thomas Theiner <thomas.theiner@gmx.de>
2026-03-13 12:14:33 +0100

    Removed files from obj-xxxx folder

--- obj-x86_64-linux-gnu/CMakeFiles/CoverFlowMPApp.dir/CoverFlowMPApp_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/CoverFlowMPApp.dir/CoverFlowMPApp_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /home/devnull69/Projects/CoverFlowMP/obj-x86_64-linux-gnu/CoverFlowMPApp_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
